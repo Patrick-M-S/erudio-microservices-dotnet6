8e1239367be54fd905f8e11e18ea7ac8b2b6e335
Patrick Mendes <patrickmendes282@gmail.com>
2022-08-09 21:54:59 -0300

    Organizing the first microservice architecture

---
+++ S08_ErudioMicroservices.NET6-OrganizingTheFirstMicrosservicesArchitect/GeekShopping/GeekShopping.ProductAPI/Migrations/20220809225302_SeedProductDataTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 1L, "T-shirt", "m has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to mak", "https://images.pexels.com/photos/1844132/pexels-photo-1844132.jpeg?cs=srgb&dl=pexels-gabriela-pereira-1844132.jpg&fm=jpg", "Name", 69.9m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 1L);
+        }
+    }
+}
